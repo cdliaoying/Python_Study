617663611fe3a29ddf8a004e9966b49fc3dbca2f
cdliaoying <simonbaker@163.com>
2018-01-24 21:43:41 +0800

    #2018.01.24 增加本地分词词库

--- Source/House_Address.py
+++ Source/House_Address.py
@@ -21,6 +21,7 @@ Description：本文档为验证地址识别和核验的业务逻辑编写
 
 import re
 import jieba
+jieba.load_userdict('/Applications/jieba-0.39/User_Define.txt') # 引用本地词库
 
 # 定义检查函数，检查地址中是否包含既定规则的字符，如果包含，拆分字符，如果不包含，提示检查地址
 def Address_First_Cut(sen):
@@ -31,20 +32,28 @@ def Address_First_Cut(sen):
     re2 = re.findall(rule2, sen)  # 查找规则2包含字符，如包含len>0。规则漏洞：若地址中非街道部分包含本关键字,可能导致结果错误
     if len(re1) > 0:
         sentence = sen.split(re1[0], 1)
-        sen1 = '%s%s' % (sentence[0], re1[0]) # %s效率较 +效率更高
-        sen2 = sentence[1]
+        sen1 = '成功'
+        sen2 = '%s%s' % (sentence[0], re1[0]) # %s效率较 +效率更高
+        sen3 = sentence[1]
     elif len(re2) > 0:
         sentence = sen.split(re2[0], 1)
-        sen1 = '%s%s' % (sentence[0], re2[0])
-        sen2 = sentence[1]
+        sen1 = '成功'
+        sen2 = '%s%s' % (sentence[0], re2[0])
+        sen3 = sentence[1]
     else:
-        sen1 = '您输入的地址不符合标准：'+sen
-    return sen1, sen2
+        sen1 = '失败'
+        sen2 = '请检查您输入的地址：'+sen
+        sen3 = ''
+    return sen1, sen2, sen3
 
 
 x = input('请输入测试地址...\n')
 y = Address_First_Cut(x)
-w1 = jieba.lcut(y[0])
-w2 = jieba.lcut(y[1])
-print(w1)
-print(w2)
+if y[0] == '成功':
+    w1 = jieba.lcut(y[1])
+    w2 = jieba.lcut(y[2])
+    print(w1)
+    print(w2)
+else:
+    print(y[1])
+
